bbb75ed9a7a34a453defb8276fc6b72891ff22d1
McKinley Harvey <mack.kinley@gmail.com>
2018-01-17 17:06:46 -0800

    edited exercises for lab practice

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -9,33 +9,44 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# You are finding the sum of numbers and Strings, which is not possible to add to each other
 
 install.packages("stringr")
+library(stringr)
 
 my.line <- "Hey, hey, this is the library"
 
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# You haven't added library yet
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# should be using variable "initials"
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
+typeof(c("dogs", "cats", "animals"))
+typeof(c(1, 4, 6))
+
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(num.1, num.2) {
+  diff <- abs((length(num.1) - length(num.2))
+  phrase <- paste("The difference in lengths is", diff)
+  return(phrase)
+  
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+vector1 <- c(1, 2, 3, 4, 5)
+vector2 <- c(1, 2, 3)
+print(CompareLength(vector1, vector2))
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
